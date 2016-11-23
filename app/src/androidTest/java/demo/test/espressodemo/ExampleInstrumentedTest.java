785c846777686ae826eb83448627bf85d05a3f27
Yuanjie Shi <380774061@qq.com>
2016-11-23 14:31:21 +0800

    Clearify testing script, delete unuse scripts

--- app/src/androidTest/java/demo/test/espressodemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/demo/test/espressodemo/ExampleInstrumentedTest.java
@@ -1,7 +1,5 @@
 package demo.test.espressodemo;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -16,7 +14,6 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -30,21 +27,12 @@ public class ExampleInstrumentedTest {
     public static final String STRING_TO_BE_TYPED_EMAIL = "EspressoDemo@mail.com";
     public static final String STRING_TO_BE_TYPED_EMAIL_PASSWORD = "123456";
 
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("demo.test.test", appContext.getPackageName());
-    }
-
-
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
             LoginActivity.class);
 
     @Test
-    public void attemptLoginTest() {
+    public void testAttemptLogin() {
         // Type text and then press the button.
         onView(withId(demo.test.espressodemo.R.id.email))
                 .perform(typeText(STRING_TO_BE_TYPED_EMAIL), closeSoftKeyboard());
